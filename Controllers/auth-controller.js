ff6b4e309c26108310d3e80d0a34340ccfb4bcbc
jztlucky6264 <77235457+jztlucky6264@users.noreply.github.com>
2022-07-05 02:47:02 +0530

    otp sms twilio commit

---
+++ Controllers/auth-controller.js
@@ -0,0 +1,37 @@
+const hashService = require("../services/hash-service");
+const otpService = require("../services/otp-service");
+
+class AuthController {
+  async sendOtp(req, res) {
+    //logic
+    const { phone } = req.body;
+    /*if (!phone) {
+      res.status(400).json({ message: "phone field is required" });
+    } */
+
+    //random number generate
+    const otp = await otpService.generateOtp();
+
+    //hashing otp
+    const timeLimit = 1000 * 60 * 2; //valid only for two minutes
+    const expires = Date.now() + timeLimit;
+    const data = `${phone}.${otp}.${expires}`;
+    const hash = hashService.hashOtp(data);
+
+    //send otp
+    try {
+      await otpService.sendBySms(phone, otp);
+      return res.json({
+        hash: `${hash}.${expires}.`,
+        phone,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "otp sending failed" });
+    }
+
+    res.json({ hash: hash });
+  }
+}
+
+module.exports = new AuthController();
---
+++ services/otp-service.js
@@ -0,0 +1,25 @@
+const crypto = require("crypto");
+const smsSid = process.env.SMS_SID;
+const smsAuthToken = process.env.SMS_AUTH_TOKEN;
+const twilio = require("twilio")(smsSid, smsAuthToken, {
+  lazyLoading: true,
+});
+
+class OtpService {
+  async generateOtp() {
+    const otp = crypto.randomInt(1000, 9999);
+    return otp;
+  }
+
+  async sendBySms() {
+    return await twilio.messages.create({
+      to: phone,
+      from: process.env.SMS_FROM_NUMBER,
+      body: `Your We-together OTP is ${otp}`,
+    });
+  }
+
+  verifyOtp() {}
+}
+
+module.exports = new OtpService();
